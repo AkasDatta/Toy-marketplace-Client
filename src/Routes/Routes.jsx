7ae11d32a5ffab86c475693236609a010963d990
Akas_Datta <012akasdatta@gmail.com>
2023-05-19 00:22:57 +0600

    added errorpage component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/home/Home";
 import Blogs from "../pages/Blogs/Blogs";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
             path: '/',
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import './ErrorPage.css'
+import error from '../../assets/oops.avif'
+
+
+const ErrorPage = () => {
+    return (
+        <div className='text-center error'>
+            <img className='errorImg' src={error} alt="" />
+            <h2>Something went wrong.</h2>
+            <h1 className='text-danger fw-bold'>404 error</h1>
+            <p>We're sorry, but the page you're looking for could not be found.</p>
+            <button className='rounded p-3 bg-danger'><Link className='no-decoration'
+                to='/'
+            >
+                <h5 className='text-light fw-bold pt-2'>Back to homepage</h5>
+          </Link></button> 
+        </div>
+    );
+};
+
+export default ErrorPage;
