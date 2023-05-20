31ced87bdbab2b192de9bc6c44026dd4d1d4b71a
Akas_Datta <012akasdatta@gmail.com>
2023-05-20 23:38:48 +0600

    added update button clicked on mytoys component then show modal with details

--- src/pages/NavigationToys/MyToys/MyToys.jsx
+++ src/pages/NavigationToys/MyToys/MyToys.jsx
@@ -2,49 +2,111 @@ import { useContext, useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 import { AuthContext } from "../../../providers/AuthProvider";
 import MyToysRow from "./MyToysRow";
-
+import Swal from "sweetalert2";
 
 const MyToys = () => {
-    const {user} = useContext(AuthContext);
-    const [addtoys, setAddtoys] = useState([]);
+  const { user } = useContext(AuthContext);
+  const [addtoys, setAddtoys] = useState([]);
 
-    const url = `http://localhost:5000/addtoys?email=${user?.email}`
-    useEffect( () => {
-        fetch(url)
+  const url = `http://localhost:5000/addtoys?email=${user?.email}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAddtoys(data));
+  }, [url]);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/addtoys/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              const remaining = addtoys.filter(
+                (addtoy) => addtoy._id !== id
+              );
+              setAddtoys(remaining);
+            }
+          })
+          .catch((error) => {
+            console.log("Error deleting toy:", error);
+          });
+      }
+    });
+  };
+
+
+  const handleConfirm = id => {
+    const proceed = confirm('Are you sure you want to update?');
+    if(proceed){
+        fetch(`http://localhost:5000/addtoys/${id}`,{
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({status: 'confirm'})
+        })
         .then(res => res.json())
-        .then(data => setAddtoys(data))
-    }, []);
+        .then(data => {
+            console.log(data);
+            if(data.modifiedCount > 0){
+                alert('Updated successfully');
+                // update state
+                const remaining = addtoys.filter(addtoy => addtoy._id !== id);
+                const updated = addtoys.find(addtoy => addtoy._id === id);
+                updated.status = "confirm"
+                const newUpdates = [updated, ...remaining];
+                setAddtoys(newUpdates);
+            }
+        })
+    }
+}
 
-    return (
-        <div className="my-5 py-5 container">
-            <h1 className="text-center my-5 fw-bold" style={{ color: '#7fa7e4' }}>My Toys Here</h1>
-            <Table striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                    <th>Photo</th>
-                    <th>Toys name</th>
-                    <th>Seller name</th>
-                    <th>Seller email</th>
-                    <th>Sub-category</th>
-                    <th>Price</th>
-                    <th>Rating</th>
-                    <th>Available quantity</th>
-                    <th>Detail description</th>
-                    <th>Update Toy</th>
-                    <th>Delete Toy</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        addtoys.map(addtoy => <MyToysRow
-                            key={addtoy._id}
-                            addtoy={addtoy}
-                        ></MyToysRow>) 
-                    }
-                </tbody>
-            </Table>
-        </div>
-    );
+  return (
+    <div className="my-5 py-5 container">
+      <h1 className="text-center my-5 fw-bold" style={{ color: "#7fa7e4" }}>
+        My Toys Here
+      </h1>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Photo</th>
+            <th>Toys name</th>
+            <th>Seller name</th>
+            <th>Seller email</th>
+            <th>Sub-category</th>
+            <th>Price</th>
+            <th>Rating</th>
+            <th>Available quantity</th>
+            <th>Detail description</th>
+            <th>Update Toy</th>
+            <th>Delete Toy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {addtoys.map((addtoy) => (
+            <MyToysRow
+              key={addtoy._id}
+              addtoy={addtoy}
+              handleDelete={handleDelete}
+              handleConfirm={handleConfirm}
+            ></MyToysRow>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default MyToys;
--- src/pages/NavigationToys/MyToys/MyToysRow.jsx
+++ src/pages/NavigationToys/MyToys/MyToysRow.jsx
@@ -1,31 +1,90 @@
-const MyToysRow = ({ addtoy }) => {
-    const { sellerName, sellerEmail, photo, price, toyname, subcategory, rating, quantity, description } = addtoy;
-    return (
-      <tr>
-        <td>
-            {photo ? (
-                <img
-                style={{ width: '45px', height: '45px' }}
-                src={photo}
-                alt="Toy"
-                />
-            ) : (
-                <p>Image not found</p>
-            )}
-        </td>
-        <td>{toyname}</td>
-        <td>{sellerName}</td>
-        <td>{sellerEmail}</td>
-        <td>{subcategory}</td>
-        <td>{price}</td>
-        <td>{rating}</td>
-        <td>{quantity}</td>
-        <td>{description}</td>
-        <td><button className='btn btn-secondary'>Update</button></td>
-        <td><button className='btn btn-secondary'>Delete</button></td>
-      </tr>
-    );
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+const MyToysRow = ({ addtoy, handleDelete, handleConfirm }) => {
+  const {
+    _id,
+    sellerName,
+    sellerEmail,
+    photo,
+    price,
+    toyname,
+    subcategory,
+    rating,
+    quantity,
+    description,
+    status,
+  } = addtoy;
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
-  
-  export default MyToysRow;
-  
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleConfirmUpdate = () => {
+    handleConfirm(_id);
+    setShowModal(false);
+  };
+
+  return (
+    <tr>
+      <td>
+        {photo ? (
+          <img style={{ width: "45px", height: "45px" }} src={photo} alt="Toy" />
+        ) : (
+          <p>Image not found</p>
+        )}
+      </td>
+      <td>{toyname}</td>
+      <td>{sellerName}</td>
+      <td>{sellerEmail}</td>
+      <td>{subcategory}</td>
+      <td>{price}</td>
+      <td>{rating}</td>
+      <td>{quantity}</td>
+      <td>{description}</td>
+      <td>
+        {status === "confirm" ? (
+          <span className="font-bold text-primary">Confirmed</span>
+        ) : (
+          <Button onClick={handleOpenModal} className="btn btn-secondary">
+            Update
+          </Button>
+        )}
+
+        {/* modal  */}
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Update Toy</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Modal content here...</p>
+            <h3>Product Price: {price}</h3>
+            <p>Product Quantity: {quantity}</p>
+            <p>Description:{description}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleConfirmUpdate}>
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </td>
+      <td>
+        <Button onClick={() => handleDelete(_id)} className="btn btn-secondary">
+          Delete
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default MyToysRow;
