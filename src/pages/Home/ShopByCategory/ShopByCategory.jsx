eae8e250364395095ee75dad4d07ef251512ac8b
Akas_Datta <012akasdatta@gmail.com>
2023-05-21 12:40:54 +0600

    modified shopbycategory component

--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -3,119 +3,128 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { Button, Card, Col, Row } from 'react-bootstrap';
 import 'react-tabs/style/react-tabs.css';
 import './ShopByCategory.css';
+import { FaRegStar, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
 
 const ShopByCategory = () => {
-    const [dogCategory, setDogCategory] = useState([]);
-    const [catCategory, setCatCategory] = useState([]);
-    const [bearCategory, setBearCategory] = useState([]);
+  const [dogToys, setDogToys] = useState([]);
+  const [catToys, setCatToys] = useState([]);
+  const [bearToys, setBearToys] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/category');
+        const data = await response.json();
+        setDogToys(data.filter((item) => item.subcategory === 'Dog Toys'));
+        setCatToys(data.filter((item) => item.subcategory === 'Cat Toys'));
+        setBearToys(data.filter((item) => item.subcategory === 'Bear Toys'));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    useEffect(() => {
-        fetch('http://localhost:5000/category/dogtoys')
-          .then((res) => res.json())
-          .then((data) => setDogCategory(data));
-    }, []);
-    useEffect(() => {
-        fetch('http://localhost:5000/category/cattoys')
-          .then((res) => res.json())
-          .then((data) => setCatCategory(data));
-    }, []);
-    useEffect(() => {
-        fetch('http://localhost:5000/category/beartoys')
-          .then((res) => res.json())
-          .then((data) => setBearCategory(data));
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <div className="container">
-            <h1 className="text-center my-5 fw-bold" style={{ color: '#F379A7' }}>Shop By Category</h1>
+  return (
+    <div className="container py-5">
+      <h1 className="text-center my-5 fw-bold" style={{ color: '#F379A7' }}>
+        Shop By Category
+      </h1>
 
-            <Tabs>
-                <div className='text-center mb-5'>
-                    <TabList>
-                        <Tab>Cat Toys</Tab>
-                        <Tab>Dog Toys</Tab>
-                        <Tab>Bear Toys</Tab>
-                    </TabList>
-                </div>
-
-                <TabPanel>
-                    <div className="container">
-                        <Row xs={1} md={2} className="g-4">
-                        {catCategory.slice(0, 2).map((category, index) => (
-                            <Col key={category.id}>
-                            <Card className="w-100">
-                                <Card.Img data-aos="zoom-in" variant="top" src={category.picture} />
-                                <Card.Body>
-                                <Card.Title> <h2><b>{category.toyName}</b></h2>
-                                </Card.Title>
-                                <Card.Text>
-                                    <h5><b>Price: </b>{category.price}</h5>
-                                </Card.Text>
-                                <Card.Text>
-                                    <h5><b>Rating:</b> {category.rating}</h5>
-                                </Card.Text>
-                                <Button variant="primary" className='shopbutton btn btn-light mb-5'>View Details</Button>
-                                </Card.Body>
-                            </Card>
-                            </Col>
-                        ))}
-                        </Row>
-                    </div>
-                </TabPanel>
-
-                <TabPanel>
-                    <div className="container">
-                        <Row xs={1} md={2} className="g-4">
-                        {dogCategory.slice(0, 2).map((category, index) => (
-                            <Col key={category.id}>
-                            <Card className="w-100">
-                                <Card.Img data-aos="zoom-in" variant="top" src={category.picture} />
-                                <Card.Body>
-                                <Card.Title> <h2><b>{category.toyName}</b></h2>
-                                </Card.Title>
-                                <Card.Text>
-                                    <h5><b>Price: </b>{category.price}</h5>
-                                </Card.Text>
-                                <Card.Text>
-                                    <h5><b>Rating:</b> {category.rating}</h5>
-                                </Card.Text>
-                                <Button variant="primary" className='shopbutton btn btn-light mb-5'>View Details</Button>
-                                </Card.Body>
-                            </Card>
-                            </Col>
-                        ))}
-                        </Row>
-                    </div>
-                </TabPanel>
-
-                <TabPanel>
-                    <div className="container">
-                        <Row xs={1} md={2} className="g-4">
-                        {bearCategory.slice(0, 2).map((category, index) => (
-                            <Col key={category.id}>
-                            <Card className="w-100">
-                                <Card.Img data-aos="zoom-in" variant="top" src={category.picture} />
-                                <Card.Body>
-                                <Card.Title> <h2><b>{category.toyName}</b></h2>
-                                </Card.Title>
-                                <Card.Text>
-                                    <h5><b>Price: </b>{category.price}</h5>
-                                </Card.Text>
-                                <Card.Text>
-                                    <h5><b>Rating:</b> {category.rating}</h5>
-                                </Card.Text>
-                                <Button variant="primary" className='shopbutton btn btn-light mb-5'>View Details</Button>
-                                </Card.Body>
-                            </Card>
-                            </Col>
-                        ))}
-                        </Row>
-                    </div>
-                </TabPanel>
-            </Tabs>
+      <Tabs>
+        <div className="text-center mb-5">
+          <TabList>
+            <Tab>Dog Toys</Tab>
+            <Tab>Cat Toys</Tab>
+            <Tab>Bear Toys</Tab>
+          </TabList>
         </div>
-    );
+
+        <TabPanel>
+          <div className="container">
+            <Row xs={1} md={2} className="g-4">
+              {dogToys.slice(0, 2).map((toy, index) => (
+                <Col key={index}>
+                  <Card className="w-100">
+                    <Card.Img data-aos="zoom-in" variant="top" src={toy.picture} />
+                    <Card.Body>
+                      <Card.Title> {toy.toyName}</Card.Title>
+                        <b>Price:</b> ${toy.price} <br />
+                        <p><b>Ratings:</b> <Rating
+                            placeholderRating={toy.rating}
+                            readonly
+                            emptySymbol={<FaRegStar></FaRegStar>}
+                            placeholderSymbol={<FaStar className='text-danger'></FaStar>}
+                            fullSymbol={<FaStar></FaStar>}
+                        ></Rating> {toy.rating}
+                        </p>
+                      <Button className='shopbutton' variant="primary">View Details</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </TabPanel>
+
+        <TabPanel>
+          <div className="container">
+            <Row xs={1} md={2} className="g-4">
+              {catToys.slice(0, 2).map((toy, index) => (
+                <Col key={index}>
+                <Card className="w-100">
+                  <Card.Img data-aos="zoom-in" variant="top" src={toy.picture} />
+                  <Card.Body>
+                    <Card.Title> {toy.toyName}</Card.Title>
+                      <b>Price:</b> ${toy.price} <br />
+                      <p><b>Ratings:</b> <Rating
+                            placeholderRating={toy.rating}
+                            readonly
+                            emptySymbol={<FaRegStar></FaRegStar>}
+                            placeholderSymbol={<FaStar className='text-danger'></FaStar>}
+                            fullSymbol={<FaStar></FaStar>}
+                        ></Rating> {toy.rating}
+                        </p>
+                    <Button className='shopbutton' variant="primary">View Details</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              ))}
+            </Row>
+          </div>
+        </TabPanel>
+
+        <TabPanel>
+          <div className="container">
+            <Row xs={1} md={2} className="g-4">
+              {bearToys.slice(0, 2).map((toy, index) => (
+               <Col key={index}>
+                <Card className="w-100">
+                    <Card.Img data-aos="zoom-in" variant="top" src={toy.picture} />
+                    <Card.Body>
+                    <Card.Title> {toy.toyName}</Card.Title>
+                        <b>Price:</b> ${toy.price} <br />
+                        <p><b>Ratings:</b> <Rating
+                            placeholderRating={toy.rating}
+                            readonly
+                            emptySymbol={<FaRegStar></FaRegStar>}
+                            placeholderSymbol={<FaStar className='text-danger'></FaStar>}
+                            fullSymbol={<FaStar></FaStar>}
+                        ></Rating> {toy.rating}
+                        </p>
+                    <Button className='shopbutton' variant="primary">View Details</Button>
+                    </Card.Body>
+                </Card>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ShopByCategory;
