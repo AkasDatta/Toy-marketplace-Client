3ae3312be59e4d4b05be52bcd359455a3a4fe423
Akas_Datta <012akasdatta@gmail.com>
2023-08-26 13:44:44 +0600

    loader added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,13 +2,29 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
 import './Main.css';
+import { useEffect, useState } from "react";
 
 const Main = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+        setIsVisible(false);
+        }, 3000);
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
         <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+        {isVisible ? <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="spinner-grow text-danger h-16 w-16 border-t-2 border-blue-500" role="status"></div>
+            <div className="spinner-grow text-warning h-16 w-16 mx-2 border-t-2 border-blue-500" role="status"></div>
+            <div className="spinner-grow text-info h-16 w-16 border-t-2 border-blue-500" role="status"></div>
+            </div> : <div>
+                    <Navbar></Navbar>
+                    <Outlet></Outlet>
+                    <Footer></Footer>
+            </div> }
         </div>
     );
 };
